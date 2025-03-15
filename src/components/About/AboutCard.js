edbd37745873b61daf75ae101a924c29956cddd4
Lohitaksh Verma <vlohitaksh@gmail.com>
2025-03-15 11:55:10 +0530

    Changes in About and Home Page

--- src/components/About/AboutCard.js
+++ src/components/About/AboutCard.js
@@ -8,16 +8,16 @@ function AboutCard() {
       <Card.Body>
         <blockquote className="blockquote mb-0">
           <p style={{ textAlign: "justify" }}>
-            Hi Everyone, I am <span className="purple">Soumyajit Behera </span>
-            from <span className="purple"> Bhubaneswar, India.</span>
+            Hi Everyone, I am <span className="purple">Lohitaksh Verma </span>
+            from <span className="purple"> Jaipur(Raj), India.</span>
             <br />
-            I am currently employed as a software developer at Juspay.
+            I am currently working on my DevOps and AWS skills.
             <br />
-            I have completed Integrated MSc (IMSc) in Maths and Computing at BIT
+            I have completed B.Tech, in Computer Sciece and Technology form Global Institute of Technology
             Mesra.
             <br />
             <br />
-            Apart from coding, some other activities that I love to do!
+            Apart from DevOps, some other activities that I love to do!
           </p>
           <ul>
             <li className="about-activity">
@@ -34,7 +34,7 @@ function AboutCard() {
           <p style={{ color: "rgb(155 126 172)" }}>
             "Strive to build things that make a difference!"{" "}
           </p>
-          <footer className="blockquote-footer">Soumyajit</footer>
+          <footer className="blockquote-footer">Lohitaksh</footer>
         </blockquote>
       </Card.Body>
     </Card>
